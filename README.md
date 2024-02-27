83f069b5f4afc849d7ba55d2a4ec4a8c1e90a42f
rocketeli1 <peschwarz4@gmail.com>
2024-02-26 23:20:04 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 20240226_portfolio
 # 20240226_Portfolio
+# 20240226_portfolio
---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+SENDER_EMAIL = os.environ.get('SENDER_EMAIL')
+RECEIVER_EMAIL = os.environ.get('RECEIVER_EMAIL')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+@app.route('/')          
+def hello_world():
+    return render_template('index.html')
+
+# other routes...
+@app.route('/tableau')
+def tableau():
+    return render_template('tableau.html')
+
+@app.route('/aboutMe')
+def aboutMe():
+    return render_template('aboutme.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+# contact form submission
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        date = request.form['date']
+
+        msg = MIMEMultipart()
+        msg['From'] = SENDER_EMAIL
+        msg['To'] = RECEIVER_EMAIL
+        msg['Subject'] = "New Contact Form Submission"
+
+        body = f"Name: {name}\nEmail: {email}\nMessage: {message}\nDate: {date}"
+        msg.attach(MIMEText(body, 'plain'))
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(SENDER_EMAIL, EMAIL_PASSWORD)
+        text = msg.as_string()
+        server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, text)
+        server.quit()
+
+        return render_template('contact.html')
+
+    return render_template('contact.html')
+
+if __name__=="__main__":   
+    app.run(debug=True)
